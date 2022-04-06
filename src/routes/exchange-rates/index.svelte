3425b1cf7bebbdd22a2bc1f3df38d0305325b147
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2022-04-06 17:53:53 +0900

    Added chart.js

--- src/routes/exchange-rates/index.svelte
+++ src/routes/exchange-rates/index.svelte
@@ -23,6 +23,7 @@
 
 <script lang="ts">
 	import { currentPageStore } from '$stores/current-page-store';
+	import LineChart from '$shared/line-chart.svelte';
 	import { onMount } from 'svelte';
 
 	export let container = 'default-container';
@@ -32,11 +33,25 @@
 		currentPageStore.set('Exchange-Rates');
 	});
 
+	const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+	const dataTest = {
+		labels: labels,
+		datasets: [
+			{
+				label: 'My First dataset',
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+				data: [0, 10, 5, 2, 20, 30, 45]
+			}
+		]
+	};
+
 	//TODO Shoul display chart
-	
 </script>
 
 <div class={container}>
 	<h1 class="text-3xl font-bold">Exchange Rates</h1>
+	<LineChart data={dataTest} />
 	<p class="break-words">{JSON.stringify(data)}</p>
 </div>
